c73475bbb8614cc3b6dd04c47afa94f5cb7cf241
Andy <lun9nin@inbox.ru>
2023-07-04 14:21:37 +0300

    Fix news demo data. Add dialog logic. Add for NewsListFullComponent open news feature

--- src/components/AppNewsListFullComponent.vue
+++ src/components/AppNewsListFullComponent.vue
@@ -10,15 +10,41 @@
         <v-divider v-if="index"></v-divider>
 
         <v-list-item>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
+          <v-list-item-title>
+            <a @click="toggleDialog(item)" class="v-btn">{{ item.title }}</a></v-list-item-title>
           <br>
           <v-list-item-subtitle>{{ item.content }}</v-list-item-subtitle>
           <br>
-          <span>{{item.author}} {{ dates.getHumanDate(item.date) }}</span>
+          <span>{{ item.author }} {{ dates.getHumanDate(item.date) }}</span>
         </v-list-item>
       </template>
 
     </v-list>
+
+    <app-dialog-component v-if="show"
+                          :show="show"
+                          @toggleDialog="toggleDialog">
+      <v-card>
+        <v-card-item>
+          <v-toolbar density="compact">
+            <v-toolbar-title>{{ post?.title }}
+            </v-toolbar-title>
+            <template v-slot:append>
+              <v-btn icon="mdi-close"
+                     color="blue"
+                     rounded
+                     @click="toggleDialog"
+                     density="compact"></v-btn>
+            </template>
+          </v-toolbar>
+
+        </v-card-item>
+        <v-card-item>
+          <v-card-text>{{ post?.content }}</v-card-text>
+        </v-card-item>
+
+      </v-card>
+    </app-dialog-component>
   </v-card>
 </template>
 
@@ -26,14 +52,23 @@
 import news from '@/demo/news.json'
 import type { News } from "@/models/news";
 import { useDate } from "@/composable/AppDateComposble";
-import { computed } from "vue";
+import { computed, ref } from "vue";
+import AppDialogComponent from "@/components/UI/AppDialogComponent.vue";
+import { Post } from "@/models/post";
 
 const newsItems = news as News[];
 const dates = useDate();
+const show = ref(false);
+const post = ref(null);
 
 const newsByDate = computed(() => {
-  return newsItems.sort((a, b) => b.date -a.date);
+  return newsItems.sort((a, b) => b.date - a.date);
 });
+
+function toggleDialog(item?: Post) {
+  show.value = !show.value;
+  post.value = item || null;
+}
 </script>
 
 <style scoped>
---
+++ src/components/UI/AppDialogComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-dialog v-model="showModal">
+    <slot></slot>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+const { show } = defineProps([ 'show' ]);
+const showModal = ref(show);
+const emits = defineEmits([ 'toggleDialog' ]);
+
+function toggleDialog() {
+  return emits('toggleDialog', showModal.value);
+}
+
+watch(showModal, () => {
+  toggleDialog();
+})
+</script>
+
+<style scoped>
+
+</style>

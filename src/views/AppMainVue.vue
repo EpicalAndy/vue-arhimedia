da27e60cf020c30103a22d406194621b6c52b77f
Andy <lun9nin@inbox.ru>
2023-07-04 14:36:28 +0300

    Add date for posts demo. Add sort by date for news and articles on main page

--- src/views/AppMainVue.vue
+++ src/views/AppMainVue.vue
@@ -3,14 +3,15 @@
     <v-row class="justify-center mt-16">
       <v-col cols="8">
         <app-content-carousel-component title="Статьи"
-                                        :items="posts"></app-content-carousel-component>
+                                        :items="posts">
+        </app-content-carousel-component>
       </v-col>
     </v-row>
 
     <v-row class="justify-center mt-16">
       <v-col cols="8">
         <app-content-carousel-component title="Последние новости"
-                                        :items="news">
+                                        :items="lastNews">
         </app-content-carousel-component>
       </v-col>
     </v-row>
@@ -22,6 +23,23 @@
 import news from '@/demo/news.json'
 import posts from '@/demo/posts.json'
 import AppContentCarouselComponent from "@/components/AppContentCarouselComponent.vue";
+import { computed } from "vue";
+import { Post } from "@/models/post";
+
+const lastNews = computed(() => {
+  return getLatestItems(news, 5);
+});
+
+const lastArticles = () => {
+  return getLatestItems(posts, 5);
+}
+
+function getLatestItems(items: Post[], count: number) {
+  const sortedItems = items.sort((a, b) => b.date - a.date);
+  const _count = count || 10;
+
+  return sortedItems.length > _count ? sortedItems.slice(0, _count) : sortedItems;
+}
 </script>
 
 <style scoped>

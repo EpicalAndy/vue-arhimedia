05a5b5ca6ec240202792c05fb2c14f96191d795a
Andy <lun9nin@inbox.ru>
2023-07-04 12:07:25 +0300

    Add ContentCarouselComponent. Refactor AppMainView component. Update site title

---
+++ src/components/AppContentCarouselComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card height="400">
+    <v-card-title>
+      <v-card-text>
+        <h2>{{ title }}</h2></v-card-text>
+    </v-card-title>
+    <v-card-item>
+      <v-carousel height="300">
+        <v-carousel-item v-for="item in items">
+          <v-sheet class="w-50 mx-auto h-auto">
+            <div style="height: 20%">
+              <h2>{{ item.title }} {{ }}</h2>
+            </div>
+            <br>
+            <div style="height: 80%" class="card-text">{{ item.content }}</div>
+          </v-sheet>
+        </v-carousel-item>
+        <template v-slot:prev="{props}">
+          <v-btn color="blue"
+                 icon="mdi-chevron-left"
+                 @click="props.onClick">
+          </v-btn>
+        </template>
+        <template v-slot:next="{props}">
+          <v-btn color="blue"
+                 icon="mdi-chevron-right"
+                 @click="props.onClick">
+          </v-btn>
+        </template>
+      </v-carousel>
+    </v-card-item>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+const { items, title } = defineProps([ 'items', 'title' ]);
+</script>
+
+<style scoped>
+.card-text {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: -webkit-box !important;
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
+  white-space: normal;
+}
+</style>
--- src/views/AppMainVue.vue
+++ src/views/AppMainVue.vue
@@ -1,65 +1,29 @@
 <template>
-  <v-row>
-    <v-col cols="8">
-      <v-card height="400">
-        <v-card-title>
-          <v-card-text>
-            <h2>Последние новости</h2></v-card-text>
-        </v-card-title>
-        <v-card-item>
-          <v-carousel height="300">
-            <v-carousel-item v-for="item in news">
-              <v-sheet class="w-50 mx-auto h-auto">
-                <div style="height: 20%">
-                  <h2>{{ item.title }} {{ }}</h2>
-                </div>
-                <br>
-                <div style="height: 80%" class="card-text">{{ item.content }}</div>
-              </v-sheet>
-            </v-carousel-item>
-          </v-carousel>
-        </v-card-item>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row class="justify-center mt-16">
+      <v-col cols="8">
+        <app-content-carousel-component title="Статьи"
+                                        :items="posts"></app-content-carousel-component>
+      </v-col>
+    </v-row>
 
-  <v-row class="justify-end">
-    <v-col cols="8">
-      <v-card height="400">
-        <v-card-title>
-          <v-card-text>
-            <h2>Последние статьи</h2></v-card-text>
-        </v-card-title>
-        <v-card-item>
-          <v-carousel height="300">
-            <v-carousel-item v-for="item in post">
-              <v-sheet class="w-50 mx-auto h-auto">
-                <div style="height: 20%">
-                  <h2>{{ item.title }} {{ }}</h2>
-                </div>
-                <br>
-                <div style="height: 80%" class="card-text">{{ item.content }}</div>
-              </v-sheet>
-            </v-carousel-item>
-          </v-carousel>
-        </v-card-item>
-      </v-card>
-    </v-col>
-  </v-row>
+    <v-row class="justify-center mt-16">
+      <v-col cols="8">
+        <app-content-carousel-component title="Последние новости"
+                                        :items="news">
+        </app-content-carousel-component>
+      </v-col>
+    </v-row>
+  </div>
+
 </template>
 
 <script setup lang="ts">
 import news from '@/demo/news.json'
-import post from '@/demo/posts.json'
+import posts from '@/demo/posts.json'
+import AppContentCarouselComponent from "@/components/AppContentCarouselComponent.vue";
 </script>
 
 <style scoped>
-  .card-text {
-    text-overflow:ellipsis;
-  overflow:hidden;
-  display: -webkit-box !important;
-  -webkit-line-clamp: 5;
-  -webkit-box-orient: vertical;
-  white-space: normal;
-  }
+
 </style>

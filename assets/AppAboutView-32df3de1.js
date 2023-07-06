5cb6d3c81a7c55f455548a2ba405bf7927504bf6
Andy <lun9nin@inbox.ru>
2023-07-06 23:28:36 +0300

    Build

---
+++ assets/AppAboutView-32df3de1.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,e as o}from"./index-5477e45a.js";const t={};function r(n,s){return c(),o("div")}const a=e(t,[["render",r]]);export{a as default};
--- assets/AppAboutView-c294ca86.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,e as o}from"./index-7176b4a5.js";const t={};function r(n,s){return c(),o("div")}const a=e(t,[["render",r]]);export{a as default};

365284801970b93b1775af0d43a73355090da480
Andy <lun9nin@inbox.ru>
2023-11-26 15:06:20 +0300

    Add layouts

---
+++ src/layouts/AppTwoColumnsLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid="">
+    <v-row>
+
+      <v-col cols="9">
+        <slot name="left">
+        </slot>
+      </v-col>
+
+      <v-col cols="3">
+        <slot name="right">
+        </slot>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
